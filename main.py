4443ec8f9798024c0bd59ff447af8c0d7d61fa8f
Anna Mikhaleva <85441714+MikhalevaAnna@users.noreply.github.com>
2025-10-31 23:22:11 +0300

    Внесла изменения в скрипт

--- main.py
+++ main.py
@@ -3,54 +3,87 @@ from pymongo import MongoClient
 import json
 from datetime import datetime, timedelta
 
-current_date = datetime.now()
 
 try:
     client = MongoClient("mongodb://localhost:27017/")
     db = client["my_database"]
-    collection = db["user_events"]
-    archived_collection = db['archived_users']
+    user_events = db["user_events"]
+    archived_users = db['archived_users']
 except pymongo.errors.ConnectionFailure:
     print("Не удалось подключиться к MongoDB!")
 except Exception as e:
     print(f"Ошибка при подключении к MongoDB: {e}")
 
 
-def archive_inactive_users(registration_days: int=30, inactive_days: int=14) -> dict:
+def archive_inactive_users(current_date: datetime, registration_days: int = 30, inactive_days: int = 14) -> dict:
     """
-    Архивирует ID неактивных пользователей и формирует по ним информацию для отчета
+    Архивирует неактивных пользователей и формирует по ним информацию для отчета
     """
-    registration_threshold = current_date - timedelta(days=registration_days)
-    activity_threshold = current_date - timedelta(days=inactive_days)
-    # Формируется запрос для поиска неактивных пользователей
-    query = {
-        "event_time": {"$lt": activity_threshold},
-        "user_info.registration_date": {"$lt": registration_threshold}
-    }
-    # Находятся пользователи для архивации
-    users_to_archive = list(collection.find(query))
-    # Пользователи архивируются
-    if users_to_archive:
-        archived_collection.insert_many(users_to_archive)
-        # Неактивные пользователи удаляются из основной коллекции
-        collection.delete_many(query)
+    registration_threshold = current_date - timedelta(days = registration_days)
+    activity_threshold = current_date - timedelta(days = inactive_days)
+    # Находятся ID пользователей, у которых последнее событие было более 14 дней назад и
+    # регистрация больше 30 дней назад
+    query  = [
+        {
+            "$group": {
+                "_id": "$user_id",
+                "last_activity": {"$max": "$event_time"},
+                "registration_date": {"$first": "$user_info.registration_date"}
+            }
+        },
+        {
+            "$match": {
+                "last_activity": {"$lt": activity_threshold},
+                "registration_date": {"$lt": registration_threshold}
+            }
+        },
+        {
+            "$sort": {
+                "_id": 1,
+                "last_activity": -1
+            }
+        }
+    ]
+
+    users_to_archive = list(user_events.aggregate(query))
+    if not users_to_archive:
+        return {
+            "date": current_date.strftime('%Y-%m-%d'),
+            "archived_users_count": 0,
+            "archived_user_ids": []
+        }
+    try:
+        archived_user_ids = []
+        for user in users_to_archive:
+            archived_user = user_events.find_one({"user_id": user['_id']})
+            if archived_user:
+                # Документы добавляются в архив
+                 archived_users.insert_one(archived_user)
+                # Добавляются в список user_id для отчета
+                 archived_user_ids.append(user['_id'])
+        # Удаляются из основной коллекции
+        user_events.delete_many({"user_id": {"$in": archived_user_ids}})
+    except Exception as e:
+        print(f"Ошибка при архивации: {e}")
+        return {"error": f"Archive failed: {e}"}
+
     report = {
-         "date": current_date.strftime('%Y-%m-%d'),
-         "archived_users_count": len(users_to_archive),
-         "archived_user_ids": list(map(lambda user: user["user_id"], users_to_archive))
+        "date": current_date.strftime('%Y-%m-%d'),
+        "archived_users_count": len(archived_user_ids),
+        "archived_user_ids": archived_user_ids
     }
     return report
 
 
-def save_report_to_file(report: dict) -> None:
+def save_report_to_file(current_date: datetime, report: dict) -> None:
     """
     Сохраняет отчет в JSON файл
     """
     try:
         report_filename = f"archive_report_{current_date.strftime('%Y-%m-%d')}.json"
 
-        with open(report_filename, 'w', encoding='utf-8') as report_out:
-            json.dump(report, report_out, indent=2, ensure_ascii=False)
+        with open(report_filename, 'w', encoding = 'utf-8') as report_out:
+            json.dump(report, report_out, indent = 2, ensure_ascii = False)
 
         print(f"Всего перемещено в архив {len(report['archived_user_ids'])} неактивных пользователей.")
         print(f"Отчет сохранен в файл: {report_filename}")
@@ -64,5 +97,9 @@ def save_report_to_file(report: dict) -> None:
 
 
 if __name__ == "__main__":
-    data = archive_inactive_users()
-    save_report_to_file(data)
+    current_date = datetime.now()
+    report_data = archive_inactive_users(current_date)
+    if 'error' not in report_data:
+        save_report_to_file(current_date, report_data)
+    else:
+        print(f"Ошибка архивации: {report_data['error']}")
